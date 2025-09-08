43a04a09e81a410fe8781fc86f7f10ef01f02e50
TheRealQi <therealqi@hotmail.com>
2025-09-08 11:57:53 +0300

    Init Commit

---
+++ src/main/java/com/q/colabtaskmanagement/ColabTaskManagementApplication.java
@@ -0,0 +1,13 @@
+package com.q.colabtaskmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColabTaskManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColabTaskManagementApplication.class, args);
+	}
+
+}
