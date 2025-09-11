91e062bdc7ab0d2ef952c836aaaf96c8799610a5
TheRealQi <therealqi@hotmail.com>
2025-09-11 20:58:23 +0300

    Done base Login Service method.

---
+++ src/main/java/com/q/colabtaskmanagement/common/dto/authentication/LoginRequestDTO.java
@@ -0,0 +1,15 @@
+package com.q.colabtaskmanagement.common.dto.authentication;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequestDTO {
+    private String usernameOrEmail;
+    private String password;
+}
